6f5074b2d23db9cdbc338c0dcf20ac751223b192
Graeme Gillies <graeme.a.gillies@gmail.com>
2024-01-11 14:11:24 +0000

    added two prompts

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-let age = 15;
+let age = 14;
 let movieRating = "15";
 
-if (age > 15 && age < 18) {
+if (age == 15 && age < 18) {
   console.log("You can watch all movies apart from 18 rated");
 } else if (age === movieRating) {
   console.log("You're just old enough to watch this movie");
 } else {
   console.log("You can't watch this movie yet");
 }
+let myName = prompt("What is your name?");
+console.log(myName);
+
+let myFavNumber = prompt("What is your Fav Number");
+console.log(myFavNumber);
