8cdec01c6bf1fff1ff3a7ee83dda3d23aa274552
Graeme Gillies <graeme.a.gillies@gmail.com>
2024-01-11 13:52:58 +0000

    initial commit on conditional

---
+++ app.js
@@ -0,0 +1,10 @@
+let age = 15;
+let movieRating = "15";
+
+if (age > 15 && age < 18) {
+  console.log("You can watch all movies apart from 18 rated");
+} else if (age === movieRating) {
+  console.log("You're just old enough to watch this movie");
+} else {
+  console.log("You can't watch this movie yet");
+}
